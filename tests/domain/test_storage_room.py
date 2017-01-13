e9af24615a30f481f30ca7e82c4fdcb22e5a58ec
Nicolas Agustin Torres <nicolastrres@gmail.com>
2017-01-12 23:53:20 -0300

    Rename storageroom to storage_room

---
+++ tests/domain/test_storage_room.py
@@ -0,0 +1,19 @@
+import uuid
+
+from rentomatic.domain.storage_room import StorageRoom
+
+
+def test_storageroom_model_init():
+    code = uuid.uuid4()
+    storage_room = StorageRoom(
+        code,
+        size=200,
+        price=10,
+        longitude='-0.08392555',
+        latitude='34.75637280'
+    )
+    assert storage_room.code == code
+    assert storage_room.size == 200
+    assert storage_room.price == 10
+    assert storage_room.longitude == '-0.08392555'
+    assert storage_room.latitude == '34.75637280'
