ff91f5aba0d1db1906a8340aaea4519052b67ec0
Nicolas Agustin Torres <nicolastrres@gmail.com>
2017-01-13 00:09:17 -0300

    Add pyhamcrest to create beautiful asserts

--- tests/domain/test_storage_room.py
+++ tests/domain/test_storage_room.py
@@ -1,5 +1,7 @@
 import uuid
 
+from hamcrest import assert_that, equal_to
+
 from rentomatic.domain.storage_room import StorageRoom
 
 
@@ -12,11 +14,11 @@ def test_storageroom_model_init():
         longitude=-0.08392555,
         latitude=34.75637280
     )
-    assert storage_room.code == code
-    assert storage_room.size == 200
-    assert storage_room.price == 10
-    assert storage_room.longitude == -0.08392555
-    assert storage_room.latitude == 34.75637280
+    assert_that(storage_room.code, equal_to(code))
+    assert_that(storage_room.size, equal_to(200))
+    assert_that(storage_room.price, equal_to(10))
+    assert_that(storage_room.longitude, equal_to(-0.08392555))
+    assert_that(storage_room.latitude, equal_to(34.75637280))
 
 
 def test_storage_room_model_from_dict():
@@ -30,8 +32,8 @@ def test_storage_room_model_from_dict():
             'latitude': 34.75637280
         }
     )
-    assert storage_room.code == code
-    assert storage_room.size == 200
-    assert storage_room.price == 10
-    assert storage_room.longitude == -0.08392555
-    assert storage_room.latitude == 34.75637280
+    assert_that(storage_room.code, equal_to(code))
+    assert_that(storage_room.size, equal_to(200))
+    assert_that(storage_room.price, equal_to(10))
+    assert_that(storage_room.longitude, equal_to(-0.08392555))
+    assert_that(storage_room.latitude, equal_to(34.75637280))
